ea908e631e44c61eb2a539f03f5f260793d68581
Juan Acuna <juan0acu@gmail.com>
2023-02-14 07:08:55 -0800

    test

--- app/src/main/java/com/example/rickandmorty/data/Result.kt
+++ app/src/main/java/com/example/rickandmorty/data/Result.kt
@@ -4,4 +4,5 @@ sealed class Result<T>(val data: T? = null, val message: String? = null) {
     class Loading<T>(data: T? = null) : Result<T>(data)
     class Success<T>(data: T?) : Result<T>(data)
     class Error<T>(message: String, data: T? = null) : Result<T>(data, message)
+
 }
